e5d6f20d6120530c54e4d76605f0c7a8debcd10f
Damian <dtellerin@teco.com.ar>
2022-03-17 22:05:33 -0300

    Damian ver 1.03.01

--- src/servicios/training.js
+++ src/servicios/training.js
@@ -16,16 +16,6 @@ function verificaLink(input){
 }
 
 export default {
-    // getAll: async() => { //Devuelve todos los productos
-    //     const productos = await trainingDao.getAll()
-    //     return productos
-    // },
-
-    // getById: async(id) => { //Devuelve producto por id
-    //     const producto = await trainingDao.getById(id)
-    //     if(producto) return producto
-    //     else return null
-    // },
     
     getByQuery: async(email) => { //Devuelve producto por id
         const courses = await trainingDao.getByQuery({'email': email} )
@@ -33,49 +23,4 @@ export default {
         else return null
     }
 
-    // save: async (productoNew) => { //Agrega producto
-    //     //verifica campos
-    //     if(!productoNew.codigo) return null
-    //     if(!productoNew.nombre) return null
-    //     if(!productoNew.descripcion) return null
-    //     if(!productoNew.categoria) return null
-    //     if(!verificaLink(productoNew.foto)) return null
-    //     if(!verificaPrecio(productoNew.precio))  return null
-    //     if(!verificaStock(productoNew.stock)) return null
-        
-    //     productoNew = Object.assign({timestamp: Date.now()}, productoNew)
-    //     const id = await trainingDao.save(productoNew)
-    //     return id
-    // },
-
-    // deleteByIdProducto: async (id)=>{ //Elimina producto por id
-    //     const producto = await trainingDao.getById(id)
-    //     if(producto){
-    //         const operacion = await trainingDao.deleteById(id)
-    //         return true
-    //     } 
-    //     else return null
-    // },
-
-    // update: async (id,productoMod)=>{ //Actualiza productos por id
-    //     if(!productoMod.id) productoMod = Object.assign({id: id}, productoMod)
-
-    //     //verifica campos
-    //     if(!productoMod.id) return null
-    //     if(!productoMod.codigo) return null
-    //     if(!productoMod.timestamp) return null
-    //     if(!productoMod.nombre) return null
-    //     if(!productoMod.descripcion) return null
-    //     if(!productoMod.categoria) return null
-    //     if(!verificaLink(productoMod.foto)) return null
-    //     if(!verificaPrecio(productoMod.precio))  return null
-    //     if(!verificaStock(productoMod.stock)) return null
-        
-    //     const producto = await trainingDao.getById(id)
-    //     if(producto){
-    //         const operacion = await trainingDao.update(productoMod)
-    //         return true
-    //     }
-    //     else return null    
-    // }
 }
